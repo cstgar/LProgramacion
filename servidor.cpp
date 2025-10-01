01f113b6c3f89971fa3bb4af4ce07a9c00d03d4b
cstgar <cinthiakstg@gmail.com>
2025-10-01 03:08:27 -0700

    Practica 2.1 Sockets Se agrega codigo de Servidor en c++

--- servidor.cpp
+++ servidor.cpp
@@ -1,61 +1,85 @@
-#include <unistd.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <stdio.h>
-#include <winsock2.h> // Contiene sys/socket.h y netinet/in.h de Windows
-#include <ws2tcpip.h> // Para algunas funciones modernas como getaddrinfo
 #include <stdlib.h>
-#include <string.h>
+
 #define PORT 6505
 
-int main(int argc, char const *argv[])
-{
-
-    // INICIALIZACIÓN DE WINSOCK (NECESARIO)
+int main() {
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        fprintf(stderr, "WSAStartup falló.\n");
-        return 1;
-    }
-                
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
+    SOCKET server_fd, new_socket;
+    struct sockaddr_in address, client_addr;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
+    int opt = 1;
 
-    //Crear socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("Error al crear el socket");
-        exit(EXIT_FAILURE);
+    // Inicializar Winsock
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
+        printf("WSAStartup falló.\n");
+        return 1;
     }
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt)))
-    {
-        perror("Error al crear el socket");
-        exit(EXIT_FAILURE);
+
+    // Crear socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Error al crear socket\n");
+        WSACleanup();
+        return 1;
     }
+
+    // Reusar puerto
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
+
+    // Dirección
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("25.0.248.40");
-    address.sin_port= htons(PORT);
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) <0)  //Asociar IP y puerto  se hace cast explicito 
-    {
-        perror("Error al sociar socket a IP y puerto");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) //Poner socket en escucha para conexiones
-    {
-        perror("Error al poner el socket en escucha");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
-    {
-        perror("Error al aceptar la conexion");
-        exit(EXIT_FAILURE);
-    }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
+    address.sin_addr.s_addr = INADDR_ANY;   // Escuchar en todas las interfaces
+    address.sin_port = htons(PORT);
 
-    // LIMPIEZA DE WINSOCK (NECESARIO)
+    // Asociar socket a IP/puerto
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == SOCKET_ERROR) {
+        printf("Error en bind: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    // Escuchar conexiones
+    if (listen(server_fd, 3) == SOCKET_ERROR) {
+        printf("Error en listen: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Servidor en espera en el puerto %d...\n", PORT);
+
+    // Aceptar cliente
+    int client_size = sizeof(client_addr);
+    new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_size);
+    if (new_socket == INVALID_SOCKET) {
+        printf("Error en accept: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    // Confirmación de conexión
+    printf("Cliente conectado desde %s:%d\n",
+           inet_ntoa(client_addr.sin_addr),
+           ntohs(client_addr.sin_port));
+
+    // Recibir mensaje del cliente
+    int valread = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
+    if (valread > 0) {
+        buffer[valread] = '\0'; // Terminar cadena
+        printf("Mensaje recibido: %s\n", buffer);
+    } else {
+        printf("No se recibió ningún mensaje o conexión cerrada. Código: %d\n", WSAGetLastError());
+    }
+
+    // Cerrar sockets
+    closesocket(new_socket);
+    closesocket(server_fd);
     WSACleanup();
+
     return 0;
 }
-
