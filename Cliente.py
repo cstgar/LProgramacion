b0e39f6853b33216d2189db9e800d98d0ceb6b17
cstgar <cinthiakstg@gmail.com>
2025-09-30 23:46:17 -0700

    se modifico mensaje en de salida del cliente

--- Cliente.py
+++ Cliente.py
@@ -6,4 +6,4 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s :  #Creando socket basado en flujos
     s.connect((HOST, PORT))  #Conectandose a traves de la IP y Puerto para enviar informacion
     s.sendall(b'Mensaje de texto del cliente')  #Enviando informacion  al servidor. La letra b indica que es un objeto de bytes en vez de u
-print('Mensaje Enviado!!!')
+print('Mensaje Enviado desde esta otra compu!!!')
